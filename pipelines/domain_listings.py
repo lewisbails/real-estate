971469206e44ba02d15269e9b8f454a67bfcab4c
Lewis Bails <lewis.bails@ripjar.com>
2023-10-16 07:02:11 +0100

    Update pipeline to look at first 5 pages

--- pipelines/domain_listings.py
+++ pipelines/domain_listings.py
@@ -14,7 +14,7 @@ logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(name)s - %(leve
 
 log = logging.getLogger(__name__)
 
-DOMAIN_URL = "https://www.domain.com.au/rent/adelaide-region-sa/?sort=dateupdated-desc&page=1"
+DOMAIN_URL = "https://www.domain.com.au/rent/adelaide-region-sa/?sort=dateupdated-desc&page="
 
 HEADERS: dict[str, str] = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
@@ -53,33 +53,33 @@ def main(args):
     db = client[args.db]
     collection = db[args.collection]
 
-    # get html
-    html = requests.get(args.url, headers=HEADERS)
-
     # extract listings, transform into standardised Listing form and load into db
     scraped = 0
-    if html.ok:
-        for listing in domain_rental_listings(html):
+    for i in range(1, 5):
+        url = DOMAIN_URL + str(i)
+        # get html
+        html = requests.get(url, headers=HEADERS)
+        if html.ok:
+            for listing in domain_rental_listings(html):
+                try:
+                    collection.insert_one(listing.model_dump(by_alias=True))
+                    scraped += 1
+                except Exception:
+                    log.exception("Couldn't insert listing")
+        else:
             try:
-                collection.insert_one(listing.model_dump(by_alias=True))
-                scraped += 1
-            except Exception:
-                log.exception("Couldn't insert listing")
-    else:
-        try:
-            html.raise_for_status()
-        except requests.HTTPError:
-            log.exception(f"Couldn't reach {args.url}")
-            raise
+                html.raise_for_status()
+            except requests.HTTPError:
+                log.exception(f"Couldn't reach {url}")
+                raise
 
     log.info(f"Finished. Scraped {scraped} listings")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Scraper for Domain rental listings")
+    parser = argparse.ArgumentParser("Scraper for Domain rental listings. Will look at first 3 list-view pages of Domain.com.au")
     parser.add_argument("--uri", type=str, help="MongoDB uri")
     parser.add_argument("--db", type=str, default="real-estate", help="Database name")
     parser.add_argument("--collection", type=str, default="listings", help="Collection name")
-    parser.add_argument("--url", type=str, default=DOMAIN_URL, help="URL for Domain.com.au rental list-view")
     args = parser.parse_args()
     main(args)
